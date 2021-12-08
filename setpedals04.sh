bb5ed6bb80ff173365669a7d531f422b7ad7d846
Worik <5387413+worikgh@users.noreply.github.com>
2021-12-08 20:45:33 +1300

    Initial commit

---
+++ setpedals04.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+## Run if button pressed four times
+MOD_HOST_PEDAL_DIR=/home/patch/ModHostPedal
+PEDALS_DIR=$MOD_HOST_PEDAL_DIR/PEDALS
+LED_FLASH=$MOD_HOST_PEDAL_DIR/led_flash
+## Set up pedals
+rm $PEDALS_DIR/A
+rm $PEDALS_DIR/B
+rm $PEDALS_DIR/C
+
+runuser -u patch -- ln -s $PEDALS_DIR/RVB $PEDALS_DIR/A
+runuser -u patch -- ln -s $PEDALS_DIR/WC $PEDALS_DIR/B
+runuser -u patch -- ln -s $PEDALS_DIR/CAPS $PEDALS_DIR/C
+
+kill -HUP `cat $MOD_HOST_PEDAL_DIR/.driver.pid`
+
+$LED_FLASH 1 0.35 4
+sleep 1
+$LED_FLASH 1 0.35 4
