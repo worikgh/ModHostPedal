167b9f8673ebd73be7d29a2d6c6f4921fe7cff0f
Worik <5387413+worikgh@users.noreply.github.com>
2021-12-29 13:37:06 +1300

    Do not kill driver

--- setpedals03.sh
+++ setpedals03.sh
@@ -13,7 +13,6 @@ runuser -u patch -- ln -s $PEDALS_DIR/Straight $PEDALS_DIR/A
 runuser -u patch -- ln -s $PEDALS_DIR/GXJcm $PEDALS_DIR/B
 runuser -u patch -- ln -s $PEDALS_DIR/LeadOne $PEDALS_DIR/C
 
-kill -HUP `cat $MOD_HOST_PEDAL_DIR/.driver.pid`
 $LED_FLASH 1 0.35 3
 sleep 1
 $LED_FLASH 1 0.35 3
